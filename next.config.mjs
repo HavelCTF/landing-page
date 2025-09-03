7e38d9e013ff2fe6aac6a8e1e9a65d964af8bc9b
neo-jgrec <jean-yanis.jeffroy@epitech.eu>
2025-09-03 12:46:24 +0200

    fix(basePath): remove path

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/landing-page/',
   webpack(config) {
     // Grab the existing rule that handles SVG imports
     const fileLoaderRule = config.module.rules.find((rule) =>
